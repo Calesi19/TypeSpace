4bfdfadba006129f8eaf31302f23119156851f49
Xander Hunt <hunt.xander@gmail.com>
2022-05-24 15:17:38 -0600

    New file, makes it easier to run the server

---
+++ origin/run_the_server.py
@@ -0,0 +1,8 @@
+"""
+This file just runs the server, all it does is remove the need to type out the command.
+Just hit the "Run Python File" button and the server is up.
+Type Ctrl+C to quit the server.
+"""
+
+import os
+os.system("py manage.py runserver")
