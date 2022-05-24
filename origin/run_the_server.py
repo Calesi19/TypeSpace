33054a2d82bcbcf443e08e06c6a403db572b9eb8
Xander Hunt <hunt.xander@gmail.com>
2022-05-24 15:18:31 -0600

    undid my last commit :P

--- origin/run_the_server.py
+++
@@ -1,8 +0,0 @@
-"""
-This file just runs the server, all it does is remove the need to type out the command.
-Just hit the "Run Python File" button and the server is up.
-Type Ctrl+C to quit the server.
-"""
-
-import os
-os.system("py manage.py runserver")
