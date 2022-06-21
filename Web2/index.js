f21da9d168df4778846e0ebd7ea1225658be3729
Carlos Lespin <carlos.lespin.silva@gmail.com>
2022-06-21 14:33:55 -0600

    life interaction

--- Web2/index.js
+++ Web2/index.js
@@ -22,8 +22,14 @@ c.fillStyle = "white";
 var velocity = 1;
 
 
-
-
+function generateRandom(maxLimit = 100){
+    let rand = Math.random() * maxLimit;
+    console.log(rand); // say 99.81321410836433
+  
+    rand = Math.floor(rand); // 99
+  
+    return rand;
+  }
 
 
 class Life {
@@ -172,6 +178,49 @@ class Meteor{
     }
 
     draw(){
+        if(this.image) { 
+            this.x = this.x - (velocity * this.speeder);
+            c.font = parseInt((50)) + 'px monospace';
+            if (typeof this.image == undefined) {
+                console.log('Hey man your image is undefined again.')
+            }
+            c.drawImage(
+                    this.image, 
+                    this.x, 
+                    this.y,
+                    this.width,
+                    this.height)
+            c.fillText(this.word, this.x, this.y + 100)
+        }
+
+    getRandomWord(){
+        const words = ["HELLO", "CES", "CAR", "FRIEND", "NO", "YES", "GOODBYE"];
+        return words[Math.floor(Math.random()* words.length)];
+    }
+}
+
+class LifeBonus{
+    constructor() {
+        this.frequency = 0
+        this.word = this.getRandomWord();
+        this.speeder = (Math.floor(Math.random() * 3)) + 1
+        this.velocity = {
+        x:0,
+        y:0
+    }
+        const image = new Image()
+        image.src = './material/heart.png'
+        image.onload = () => {
+            const scale = 0.1 + (Math.random() / 4)
+            this.image = image
+            this.width = image.width * scale
+            this.height = image.height * scale
+            this.x = canvas.width - this.width + 500 
+            this.y = (canvas.height) - this.height - (Math.random() * window.innerHeight)
+        }     
+    }
+
+    draw(){
         if(this.image) 
         this.x = this.x - (velocity * this.speeder);
         c.font = parseInt((50)) + 'px monospace';
@@ -188,12 +237,13 @@ class Meteor{
         const words = ["HELLO", "CES", "CAR", "FRIEND", "NO", "YES", "GOODBYE"];
         return words[Math.floor(Math.random()* words.length)];
     }
-
-    
 } 
+
+
 class Actors {
     constructor() {
         this.meteors = [new Meteor(), new Meteor(), new Meteor()];
+        this.lives = []
     }
 
     spawnMeteor() {
@@ -205,20 +255,31 @@ class Actors {
     }
 
     checkMeteorMatch(targetWord) {
-        // console.log('checkMeteorMatch lvl 1', targetWord)
-        console.log(targetWord)
         for (let i = 0; i < this.meteors.length; i++) {
-            // console.log('checkMeteorMatch lvl 2')
             console.log(this.meteors[i].word, targetWord)
             if (this.meteors[i].word == targetWord) {
-                console.log('checkMeteorMatch lvl 3')
                 this.destroyMeteor(i)
                 this.spawnMeteor()
             }
         }
     }
 
+    spawnLife() {
+        this.lives.push(new LifeBonus())
+    }
+
+    destroyLife() {
+        this.lives.splice(index, 1);
+    }
+
+    incrementFrequency() {
+        this.frequency += 1;
+    }
+
+
 }
+
+
 
 
 class Input {
@@ -228,8 +289,6 @@ class Input {
 
     addLetter(character) {
         this.targetWord.push(character)
-        // console.log(character)
-        // console.log(this.targetWord)
     }
 
     deleteLetter() {
@@ -242,8 +301,6 @@ class Input {
         for (let character in this.targetWord) {
             current_word = current_word + this.targetWord[character];
         }
-        console.log(this.targetWord)
-        console.log(current_word)
         this.targetWord = [];
         actors.checkMeteorMatch(current_word);
     }
@@ -253,108 +310,109 @@ class Input {
         c.fillText(this.targetWord.join(""), 15, 1000)
     }
 
+    checkForInput() {
+        document.addEventListener('keydown', function(e) {
+            console.log('hey you pushed something')
+            switch(e.keyCode) {
+                case 13: // enter
+                    input.checkWord();
+                    break;
+                case 32: // spacebar
+                    input.checkWord();
+                    break;
+                case 8: // backspace
+                    input.deleteLetter();
+                    break;
+                case 65: // a
+                    input.addLetter('A')
+                    break;
+                case 66: // b
+                    input.addLetter('B')
+                    break;
+                case 67: // c
+                    input.addLetter('C')
+                    break;
+                case 68: // d
+                    input.addLetter('D')
+                    break;
+                case 69: // e
+                    input.addLetter('E')
+                    break;
+                case 70: // f
+                    input.addLetter('F')
+                    break;
+                case 71: // g
+                    input.addLetter('G')
+                    break;
+                case 72: // h
+                    input.addLetter('H')
+                    break;
+                case 73: // i
+                    input.addLetter('I')
+                    break;
+                case 74: // j
+                    input.addLetter('J')
+                    break;
+                case 75: // k
+                    input.addLetter('K')
+                    break;
+                case 76: // l
+                    input.addLetter('L')
+                    break;
+                case 77: // m
+                    input.addLetter('M')
+                    break;
+                case 78: // n
+                    input.addLetter('N')
+                    break;
+                case 79: // o
+                    input.addLetter('O')
+                    break;
+                case 80: // p
+                    input.addLetter('P')
+                    break;
+                case 81: // q
+                    input.addLetter('Q')
+                    break;
+                case 82: // r
+                    input.addLetter('R')
+                    break;
+                case 83: // s
+                    input.addLetter('S')
+                    break;
+                case 84: // t
+                    input.addLetter('T')
+                    break;
+                case 85: // u
+                    input.addLetter('U')
+                    break;
+                case 85: // v
+                    input.addLetter('V')
+                    break;
+                case 87: // w
+                    input.addLetter('W')
+                    break;
+                case 88: // x
+                    input.addLetter('X')
+                    break;
+                case 89: // y
+                    input.addLetter('Y')
+                    break;
+                case 90: // z
+                    input.addLetter('Z')
+                    break;
+            };
+        })
+    }
+
 }
 
 const input = new Input();
 const score = new Score()
 const player = new Player()
 const actors = new Actors();
-const life = new Life()
-
-
-document.addEventListener('keydown', function(e) {
-    console.log('hey you pushed something')
-    switch(e.keyCode) {
-        case 32: // enter
-            input.checkWord();
-            break;
-        case 32: // spacebar
-            input.checkWord();
-            break;
-        case 8: // backspace
-            input.deleteLetter();
-            break;
-        case 65: // a
-            input.addLetter('A')
-            break;
-        case 66: // b
-            input.addLetter('B')
-            break;
-        case 67: // c
-            input.addLetter('C')
-            break;
-        case 68: // d
-            input.addLetter('D')
-            break;
-        case 69: // e
-            input.addLetter('E')
-            break;
-        case 70: // f
-            input.addLetter('F')
-            break;
-        case 71: // g
-            input.addLetter('G')
-            break;
-        case 72: // h
-            input.addLetter('H')
-            break;
-        case 73: // i
-            input.addLetter('I')
-            break;
-        case 74: // j
-            input.addLetter('J')
-            break;
-        case 75: // k
-            input.addLetter('K')
-            break;
-        case 76: // l
-            input.addLetter('L')
-            break;
-        case 77: // m
-            input.addLetter('M')
-            break;
-        case 78: // n
-            input.addLetter('N')
-            break;
-        case 79: // o
-            input.addLetter('O')
-            break;
-        case 80: // p
-            input.addLetter('P')
-            break;
-        case 81: // q
-            input.addLetter('Q')
-            break;
-        case 82: // r
-            input.addLetter('R')
-            break;
-        case 83: // s
-            input.addLetter('S')
-            break;
-        case 84: // t
-            input.addLetter('T')
-            break;
-        case 85: // u
-            input.addLetter('U')
-            break;
-        case 85: // v
-            input.addLetter('V')
-            break;
-        case 87: // w
-            input.addLetter('W')
-            break;
-        case 88: // x
-            input.addLetter('X')
-            break;
-        case 89: // y
-            input.addLetter('Y')
-            break;
-        case 90: // z
-            input.addLetter('Z')
-            break;
-    };
-})
-
+const life = new Life();
+input.checkForInput()
 
 function update(){
     
@@ -362,15 +420,15 @@ function update(){
     c.drawImage(video,0,0,1920,1080);
 
     
-    for (var i = 0; i < actors.meteors.length; i++){
-        actors.meteors[i].draw()
-    }
-
 
     for (var i = 0; i < actors.meteors.length; i++){
         if (actors.meteors[i].x < 50) {
             actors.destroyMeteor(i)
             actors.spawnMeteor()
+            life.loseLife()
+            if (life.getLife() == 0){
+                alert("Game Over")
+            }
         }
     }
 
@@ -379,18 +437,37 @@ function update(){
         score.score += 1
     }
     
-    
+    for (var i = 0; i < actors.meteors.length; i++){
+        if (actors.meteors.length != 0) {
+            actors.meteors[i].draw()
+        }
+    }
+
+    for (var i = 0; i < actors.lives.length; i++){
+        if (actors.lives.length != 0) {
+            actors.lives[i].draw()
+        }
+    }
+
+
+    if (true){
+        actors.spawnLife();
+    }
+
+
     score.increment()
     score.draw()
     player.draw()
     velocity = velocity + .0001
-    life.draw()
+    if (life.frequency == 100) {
+        life.spawnLife()
+        life.frequency = 0;
+    }
+
     input.draw()
+    
     requestAnimationFrame(update); // wait for the browser to be ready to present another animation fram.    
     
-    
-
-
 }
 
 
